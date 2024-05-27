36b91499ee917f02a28199547f261623c7bc0b67
William Colasso <161973026+Lilam0@users.noreply.github.com>
2024-05-27 16:14:08 -0300

    Continuação

--- Content/script.js
+++ Content/script.js
@@ -1,19 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const titulo = document.getElementById('titulo');
-    const botao = document.getElementById('botao1');
-    const botao2 = document.getElementById('botao2');
 
-    botao.addEventListener('click', function() {
-        titulo.textContent = 'Você clicou no botão!';
-        document.body.style.backgroundColor = getRandomColor();
+    document.addEventListener('DOMContentLoaded', function() {
+        const botao = document.getElementById('botao1');
+        botao.addEventListener('click', function() {
+            alert('Botão clicado!');
+        });
     });
 
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-});
