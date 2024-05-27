b6885200de3268f679e5be45a9b429a05fbc9cbf
William Colasso <161973026+Lilam0@users.noreply.github.com>
2024-05-27 16:44:47 -0300

    .

--- Content/script.js
+++ Content/script.js
@@ -1,8 +1,11 @@
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const botao = document.getElementById('botao1');
-        botao.addEventListener('click', function() {
-            alert('Botão clicado!');
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const botao1 = document.getElementById('botao1');
+    botao1.addEventListener('click', function() {
+        alert('Botão "Quero criptografar" clicado!');
     });
 
+    const botao2 = document.getElementById('botao2');
+    botao2.addEventListener('click', function() {
+        alert('Botão "Quero descriptografar" clicado!');
+    });
+});
