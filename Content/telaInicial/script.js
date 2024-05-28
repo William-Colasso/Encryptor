775d4fb7e37c0f466ea3add9470c142ac64aee23
William Colasso <william.colasso@gmail.com>
2024-05-28 20:45:29 -0300

    Update script.js

--- Content/telaInicial/script.js
+++ Content/telaInicial/script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { //Quando o arquivo HTML for totalmente carregado, o comando será executado
     const botao1 = document.getElementById('botao1')
     botao1.addEventListener('click', function() {
        //Comando
     })
 
-    const botao2 = document.getElementById('botao2')
+    const botao2 = document.getElementById('botao2')//Quando o arquivo HTML for totalmente carregado, o comando será executado
     botao2.addEventListener('click', function() {
         //Comando
     })
