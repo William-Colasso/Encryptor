4d7cfe7cda8c86827c213ae8353dcda889569d0b
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-05-28 15:58:26 -0300

    Alterações

--- Content/telaInicial/script.js
+++ Content/telaInicial/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const botao1 = document.getElementById('botao1')
     botao1.addEventListener('click', function() {
-        alert('Botão  clicado!')
+       //Comando
     })
 
     const botao2 = document.getElementById('botao2')
     botao2.addEventListener('click', function() {
-        alert('Botão clicado!')
+        //Comando
     })
 })
