cd58577e98dd25f84f6f58fd929fb0a1c97efc86
William Colasso <william.colasso@gmail.com>
2024-06-04 23:02:36 -0300

    Criação do segundo Flow
    Após o usuário escolher "cifra de César"
    uma segunda barrada de rolagem será criada para ele escolher o número para a criptografia.

--- Content/criptografar/script.js
+++ Content/criptografar/script.js
@@ -50,3 +50,15 @@ function morse(){
         }
     }
 }
+
+setInterval(() => {
+    const elementoEscondido = document.getElementById('overflow_container');
+    if(cifra_cesar.checked){
+        elementoEscondido.classList.remove('falso_flow');
+        elementoEscondido.classList.add('segundo_flow');
+    }
+    else{
+        elementoEscondido.classList.remove('segundo_flow');
+        elementoEscondido.classList.add('falso_flow');
+    }
+  }, 500);
