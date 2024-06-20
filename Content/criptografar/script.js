18d380232621497c5b66e1442e93dcfedded2676
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-06-20 13:20:58 -0300

    Criando arquivos

--- Content/criptografar/script.js
+++ Content/criptografar/script.js
@@ -1,26 +1,5 @@
 
-var aes = document.getElementById('aes')
-var blow_f = document.getElementById('blowfish')
-var codigo_binario = document.getElementById('codigo-binario')
-var codigo_morse = document.getElementById('codigo-morse')
-var cifra_cesar = document.getElementById('cifra-cesar')
-var cifra_playfair = document.getElementById('cifra-playfair')
-var cifra_substituicao = document.getElementById('cifra-substituicao')
-var cifra_transposicao = document.getElementById('cifra-transposicao')
-var cifra_vigenere = document.getElementById('cifra-vigenere')
-var des = document.getElementById('des')
-var diffie_hellman = document.getElementById('diffie-hellman')
-var dsa  = document.getElementById('dsa')
-var elgamal  = document.getElementById('elgamal')
-var md5  = document.getElementById('md5')
-var rc4 = document.getElementById('rc4')
-var rsa  = document.getElementById('rsa')
-var salsa20 = document.getElementById('salsa20')
-var serpent = document.getElementById('serpent')
-var sha1  = document.getElementById('sha1')
-var sha2  = document.getElementById('sha2')
-var sha3 = document.getElementById('sha3')
-var _3des = document.getElementById('3des')
+
 
 
 
